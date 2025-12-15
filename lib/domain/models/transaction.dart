2694ae7ab24ecdd8b3968b8f8cb5f8723236f7cb
Nguyen Huy <huynguyenfw@gmail.com>
2025-12-15 07:56:59 +0700

    [feat] S1-B3-4- Implement TransactionRepository CRUD

--- lib/domain/models/transaction.dart
+++ lib/domain/models/transaction.dart
@@ -16,4 +16,48 @@ class Transaction {
     required this.createdAt,
     this.note,
   });
+
+  Transaction copyWith({
+    int? id,
+    int? amount,
+    String? type,
+    String? categoryName,
+    String? bank,
+    DateTime? createdAt,
+    String? note,
+  }) {
+    return Transaction(
+      id: id ?? this.id,
+      amount: amount ?? this.amount,
+      type: type ?? this.type,
+      categoryName: categoryName ?? this.categoryName,
+      bank: bank ?? this.bank,
+      createdAt: createdAt ?? this.createdAt,
+      note: note ?? this.note,
+    );
+  }
+
+  factory Transaction.fromMap(Map<String, dynamic> map) {
+    return Transaction(
+      id: map['id'] as int?,
+      amount: map['amount'] as int,
+      type: map['type'] as String,
+      categoryName: map['categoryName'] ?? map['category'] ?? '',
+      bank: map['bank'] as String?,
+      createdAt: DateTime.parse(map['created_at'] ?? map['createdAt'] ?? map['date_time']),
+      note: map['note'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'amount': amount,
+      'type': type,
+      'category': categoryName,
+      'bank': bank,
+      'created_at': createdAt.toIso8601String(),
+      'note': note,
+    };
+  }
 }
